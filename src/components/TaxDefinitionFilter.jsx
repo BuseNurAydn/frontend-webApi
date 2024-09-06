615f8d54b787c183f999d83bb171a6228dba9c99
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-09-06 11:55:56 +0300

    Filtreleme uyarı mesajı

--- src/components/TaxDefinitionFilter.jsx
+++ src/components/TaxDefinitionFilter.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react';
-import { TextField, Button,Grid } from '@mui/material';
+import { TextField, Button,Grid,Alert,Stack } from '@mui/material';
+import { styled } from '@mui/system';
 
-function TaxDefinitionFilter({ onFilter }) {
+const StyledAlert = styled(Alert)`
+  background-color: #2196f3; 
+  color: white;
+`;
+
+const TaxDefinitionFilter = ({ onFilter }) => {
   const [firstName, setFirstName] = useState('');
   const [legalTaxCode, setLegalTaxCode] = useState('');
   const [ratio, setRatio] = useState('');
+  const [errorMessage, setErrorMessage] = useState(''); // Uyarı mesajı için state
 
   const handleFilter = () => {
+    // Eğer tüm alanlar boşsa, bilgi getir butonuna tıklandıysa uyarı mesajı göstermek için
+    if (!firstName && !legalTaxCode && !ratio) {
+      setErrorMessage('Lütfen en az bir filtre alanını doldurun.');
+      return;
+    }
+    // Uyarı mesajını temizler ve filtreleme işlemi başlar
+    setErrorMessage('');
     onFilter({ firstName, legalTaxCode, ratio });
-              
   };
 
   return (
     <>
-    <Grid container sx={{padding:'20px'}}>
+    <Grid container sx={{padding:'10px'}}>
+      {errorMessage && (
+          <Grid item xs={12} >
+            <Stack sx={{ width: '100%' }} spacing={2}>
+              <StyledAlert severity="warning" onClose={() => setErrorMessage('')}>
+                {errorMessage}
+              </StyledAlert>
+            </Stack>
+          </Grid>
+        )}
           <TextField 
           id="firstName"
           label="Adı" 
@@ -21,7 +43,7 @@ function TaxDefinitionFilter({ onFilter }) {
           size="small" 
           value={firstName} 
           onChange={(e) => setFirstName(e.target.value)} 
-          sx={{ marginRight: 4 }} />
+          sx={{ marginRight: 4, marginTop:2 }} />
 
           <TextField 
           id="legalTaxCode" 
@@ -30,7 +52,7 @@ function TaxDefinitionFilter({ onFilter }) {
           size="small" 
           value={legalTaxCode} 
           onChange={(e) => setLegalTaxCode(e.target.value)}  
-          sx={{ marginRight: 4 }} />
+          sx={{ marginRight: 4, marginTop:2 }} />
 
           <TextField 
           id="ratio" 
@@ -40,11 +62,10 @@ function TaxDefinitionFilter({ onFilter }) {
           type="number" 
           value={ratio} 
           onChange={(e) => setRatio(e.target.value)} 
-          sx={{ marginRight: 4 }} />
-          <Button variant="contained" size="small" onClick={handleFilter}> Bilgi getir</Button>
+          sx={{ marginRight: 4, marginTop:2 }} />
+          <Button variant="contained" size="small" sx={{ marginTop:2 }} onClick={handleFilter}> Bilgi getir</Button>
       </Grid>
       </>
   );
 }
-
 export default TaxDefinitionFilter;
